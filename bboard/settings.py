fccc82b8a17d63d9978f2c2f509c1b59834dafc5
mylittlepray <tklikey@gmail.com>
2026-01-12 14:43:27 +0700

    allowed hosts

--- bboard/settings.py
+++ bboard/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['193.176.78.111', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['193.176.78.111', 'localhost', '127.0.0.1', 'myclassifieds.ru', 'www.myclassifieds.ru']
 
 ROOT_URLCONF = 'bboard.urls'
 
