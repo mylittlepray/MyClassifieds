4711c67c27812c45f59db2e9bbe6938ccddbcc86
mylittlepray <tklikey@gmail.com>
2026-01-12 19:28:22 +0700

    debug off

--- bboard/settings.py
+++ bboard/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['myclassifieds.ru', 'www.myclassifieds.ru', '193.176.78.111', 'localhost', '127.0.0.1']
 
